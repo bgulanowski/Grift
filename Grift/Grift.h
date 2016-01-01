56ac169c411ff021734c142a6e7135e961014efd
Brent Gulanowski <bgulanowski@gmail.com>
2015-12-31 20:04:47 -0500

    Initial Commit

---
+++ Grift/Grift.h
@@ -0,0 +1,19 @@
+//
+//  Grift.h
+//  Grift
+//
+//  Created by Brent Gulanowski on 2015-12-31.
+//  Copyright © 2015 Lichen Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Grift.
+FOUNDATION_EXPORT double GriftVersionNumber;
+
+//! Project version string for Grift.
+FOUNDATION_EXPORT const unsigned char GriftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Grift/PublicHeader.h>
+
+
---
+++ GriftTests/GriftTests.swift
@@ -0,0 +1,36 @@
+//
+//  GriftTests.swift
+//  GriftTests
+//
+//  Created by Brent Gulanowski on 2015-12-31.
+//  Copyright © 2015 Lichen Labs. All rights reserved.
+//
+
+import XCTest
+@testable import Grift
+
+class GriftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
